cccdb59cb82172d5b6d534b4e832736f3c75065d
Alborz Mesbahi <alborzmesbahi@WTA-MBP163.local>
2017-01-11 23:08:18 -0500

    latest changes

--- js/app.js
+++ js/app.js
@@ -20,10 +20,10 @@ var ticTacToeModule = +function () {
     startButton.addEventListener("click", function (event) {
         screenStartDiv.style.display = 'none';
         board.style.display = 'block';
-        playerListItem1.className = "players active";
+        playerListItem1.classList.add('active');
     });
 
-    // Player object. List item is the current player
+    // Player object. List item is the current player as displayed at the top left and right of screen
     function PlayerObject(name, listitem) {
         this.name = name;
         this.turn = false;
@@ -68,32 +68,63 @@ var ticTacToeModule = +function () {
         playerO.turn = false;
         // Highlight the box with O and make O active
         playerX.active = true;
-        playerListItem1.className = "players active";
+        playerListItem1.classList.add('active');
+        console.log(playerListItem1);
     } else if (playerX.turn == false) {
         playerO.turn = true;
         // Highlight the box with O and make O active
         playerX.active = false;
-        playerListItem2.className = "players active";
+        playerListItem2.classList.add('active');
     }
 
     // loop over the boxes; when user is hovering over one, add the CSS class to that box
-    // when the user clicks a box, add class to selected box and switch player
+    // when the user clicks a box, add class to selected box and switch active player and hover state
         for (var i = 0; i < boxes.length; i++) {
             var box = boxes[i];
             console.log(box);
             box.addEventListener('mouseover', function (event) {
-                if (playerX.turn) { 
+                if (playerX.turn && playerListItem1.className == 'players active') {
                     this.classList.add('xSVG');
                     this.classList.remove('oSVG');
                 }
-                if (playerO.turn) {
-                    this.classList.add('oSVG');
+                if (playerO.turn && playerListItem2.className == 'players active') {
                     this.classList.remove('xSVG');
+                    this.classList.add('oSVG');
                 }
             }, false);
 
-            box.addEventListener('click', function() {
-                
+            box.addEventListener('mouseout', function (event) {
+                if (this.className != 'box-filled-2' || this.className != 'box-filled-1') {
+                    this.classList.remove('xSVG');
+                    this.classList.remove('oSVG');
+                }
+            }, false);
+
+            box.addEventListener('click', function(event) {
+                if (playerX.turn) {
+                    if (this.className != 'box-filled-2') {
+                        // add the symbol by adding the CSS class
+                        this.classList.add('box-filled-1');
+                        // switch active players
+                        playerListItem1.classList.remove('active');
+                        playerListItem2.classList.add('active');
+                        console.log(playerO.turn);
+                        console.log(playerX.turn);
+                        console.log(playerListItem2.classList);
+                    }
+                }
+                if (playerO.turn) {
+                    if (this.className != 'box-filled-1') {
+                        // add the symbol by adding the CSS class
+                        this.classList.add('box-filled-2');
+                        // switch active players
+                        playerListItem2.classList.remove('active');
+                        playerListItem1.classList.add('active');
+                        console.log(playerX.turn);
+                        console.log(playerO.turn);
+                        console.log(playerListItem1.classList);
+                    }
+                }
             }, false);
         }
 }();
