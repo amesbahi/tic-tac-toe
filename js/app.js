9b0e2d527e355ad8e5b587984d5e72687dc666a2
Alborz Mesbahi <alborzmesbahi@WTA-MBP163.local>
2017-01-14 21:28:31 -0500

    latest changes

--- js/app.js
+++ js/app.js
@@ -159,19 +159,21 @@ var ticTacToeModule = +function () {
 
     // Loop through the winning combinations and select one to match against player's selected box array
     for (var i = 0; i < winningCombos.length; i++) {
-        var winningCombo = winningCombos[i];
-        console.log(winningCombo);
+        //var winningCombo = winningCombos[i];
+        //console.log(winningCombo);
         var counter = 0;
         // loop through the players checkedboxes array and see if the index matches one of the indexes for
         // a winning combo. If it does, increment the counter by 1. Then if the length of the counter is 
         // equal to the length of the winning combo, the player wins and game ends
-        for (var j = 0; j < (playerXCheckedBoxes + playerOCheckedBoxes).length; j++) {
-            if (playerXCheckedBoxes && winningCombo.includes((playerXCheckedBoxes + playerOCheckedBoxes).index)) {
+        for (var j = 0; j < playerXCheckedBoxes.length || j < playerOCheckedBoxes.length; j++) {
+            if (/*playerXCheckedBoxes && */winningCombos[i].includes(playerXCheckedBoxes.index) || 
+            winningCombos[i].includes(playerOCheckedBoxes.index)) {
                 // player X wins
                 //board.style.display = 'none';
                 //winScreenDiv.style.display = 'block';
                 counter++;
-            } if (counter.length === winningCombo.length) {
+                console.log(counter);
+            } if (counter.length === winningCombos[i].length) {
                 // player O wins
                 board.style.display = 'none';
                 winScreenDiv.style.display = 'block';
